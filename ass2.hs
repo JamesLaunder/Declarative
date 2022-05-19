1acbf938c9ff0b5ad5040815ab1cc0ce3a893cb3
James <jimmylaunder21@gmail.com>
2022-05-19 12:07:13 +1000

    working

--- ass2.hs
+++ ass2.hs
@@ -66,6 +66,9 @@ feedback ys xs = ( length( [x | x <- xs, y <- ys, hit y x]),
  length ( nub(nub([x | x <- xs, y <- ys, nearBy1 y x])\\ [x | x <- xs, y <- ys, hit y x])), 
  length (nub (nub ([x | x <- xs, y <- ys, nearBy2 y x]) \\ ([x | x <- xs, y <- ys, hit y x] ++ [x | x <- xs, y <- ys, nearBy1 y x]))))
 
+
+
+
 --feedback xs ys = (length [x | x <- xs, y <- ys, hit x y], 0, 0) -- this is being used to test the nextguess function so it exits when 3 found
 -- feedback xs ys = (length [x | x <- xs, y <- ys, hit x y], length (nub [x | x <- xs, y <- ys, nearBy1 x y]), length (nub [x | x <- xs, y <- ys, nearBy2 x y]))
 
@@ -75,9 +78,11 @@ initialGuess = ([Location 0 0, Location 0 0, Location 0 0], locs) -- this is the
 -- This kinda works
 -- it iterates properly but it doesnt guess every possible combination cus im dumb so it never gets it right
 nextGuess :: ([Location],GameState) -> (Int,Int,Int) -> ([Location], GameState)
--- nextGuess (xs, _) (y,z,k) = ([Location 5 0, Location 3 1, Location 6 3], ()) -- this is the correct guess for the set test case
--- nextGuess (xs, _) (y,z,k) = (take 3 (getAllLocs (last xs)), ())
-nextGuess (xs, ys) (y,z,k) = (ys !! ((length xs) `div` 3), drop 1 ys)
+-- nextGuess (xs, _) (y,z,k) = ([Location 5 0, Location 3 1, Location 6 3], []) -- this is the correct guess for the set test case
+nextGuess (xs, ys) (0,0,0) = (ys !! ((length xs) `div` 3), squares xs)
+nextGuess (xs, ys) (_,_,_) = (ys !! ((length xs) `div` 3), drop 1 ys)
+-- nextGuess (xs, ys) (y,z,k) = (take 1 ys, drop 1 ys)
+
 -- this generates 3 locations next to each other from the last guess value
 -- e.g. last guess is Location 2 0 it will generate [Location 3 1...]
 getAllLocs :: Location -> [Location]
@@ -92,13 +97,69 @@ locs = combos 3 allLocs
 allLocs :: [Location]
 allLocs = [(Location x y)| x <- [0..7], y <- [0..3]]
 
+squares :: [Location] -> [[Location]]
+squares xs = combos 3 (squaresAvailable xs)
+
+squaresAvailable :: [Location] -> [Location]
+squaresAvailable xs = [(Location x y)| x <- [0..7], y <- [0..3], (Location x y) /= head xs && (Location x y) /= last xs && (Location x y) /= (xs !! 1)]
+-- locTo :: [(Location, Location, Location)] -> [Location]
+-- locTo [(Location x y, Location x1 y1, Location x2 y2)] = [Location x y, Location x1 y1, Location x2 y2]
+
+locsTest :: [(Location, Location, Location)] -> IO ()
+locsTest xs = print (take 1 xs)
+
+allLocs1 :: [(Location, Location, Location)]
+allLocs1 = [(Location x y, Location x1 y1, Location x2 y2) | x <- [0..7], y <- [0..3], x1 <- [0..7], y1 <- [0..3], x2 <- [0..7], y2 <- [0..3], not((x == x1) || (y == y1)) && not((x == x2) || (y == y2)) && not((x1 == x2) || (y1 == y2))]
+-- allSame (Location x y) (Location x1 y1) (Location x2 y2) = (x == x1) && (y == y1) || (x == x2) && (y == y2) || (x1 == x2) && (y1 == y2)
 combos :: Int -> [Location] -> [[Location]]
 combos 0 lst = [[]]
-combos n lst = [x:xs | x <- lst, xs <- combos (n-1) lst]
+combos n lst = [(x:ys) | x:xs <- tails lst, ys <- combos (n-1) xs]
+
+-- newLocs :: [[Location]] -> [Location]
+-- newLocs xs = map testingSame3 xs
+
+-- TRYING TO ITERATE THROUGH LIST OF LSITS
+-- try2 :: [[Location]] -> Int -> [[Location]]
+-- try2 xs n =  take n xs
+
+-- try3 :: GameState -> Int
+-- try3 try count = do
+--   let try' = try2 try count
+--   try3 try' (count+1)
+   
 
 
 
+testingSame :: [[Location]] -> [Location]
+-- testingSame xs = map (map allSame xs)
+testingSame xs = [c | x <- xs, c <- x]
+-- testingSame xs = nub (testingSame2 (xs !! 0) (xs !! 0) (xs !! 0))
+-- flatLocs :: Bool
+-- flatLocs = testingSame3 (testingSame locs)
+-- flatLocs =  testingSame3 (locs !! 8000)
+-- flatLocs :: [[Location]] -> [Location]
+-- flatLocs xs = [x | x <- xs, testingSame3 x]
 
+testingSame3 :: [Location] -> Bool
+testingSame3 xs =  allSame (xs !! 0) (xs !! 1) (xs !! 2)
+
+
+
+addLists :: Num a => [[a]] -> [a]
+addLists xs = map sum . transpose $ zipWith (\n x -> replicate n 0 ++ x) [0..] xs
+
+testingSame2 :: [Location] -> [Location] -> [Location] -> [Location]
+testingSame2 xs ys zs =  [x | x <- xs, y <- ys, z <- zs, not (allSame x y z)]
+-- testingSame2 :: [Location] -> [Location] -> [Location] -> [Location]
+-- testingSame2 xs ys zs  =  [x | x <- xs, not (allSame x)]
+
+allSame :: Location -> Location -> Location -> Bool
+allSame (Location x y) (Location x1 y1) (Location x2 y2) = (x == x1) && (y == y1) || (x == x2) && (y == y2) || (x1 == x2) && (y1 == y2)
+
+every :: Int -> [Location] -> [Location]
+every n xs = case drop (n-1) xs of
+              y : ys -> y : every n ys
+              [] -> []
 -- THE LINE BELOW WORKS FOR TEST CASE 
 -- feedback [(Location 0 0), (Location 3 1), (Location 1 2)] [(Location 3 1), (Location 1 2), (Location 0 0)]
 -- RETURNS (3,0,3)
